f606b252600e828d1a58beba432feabc5581114d
HugoDonator <hdonator15@gmail.com>
2026-01-24 18:32:48 -0400

    feat: Auto-detect WABA ID from Phone Number ID when syncing templates

--- backend/src/services/WhatsappTemplateServices/SyncWhatsappTemplatesService.ts
+++ backend/src/services/WhatsappTemplateServices/SyncWhatsappTemplatesService.ts
@@ -47,9 +47,41 @@ const SyncWhatsappTemplatesService = async ({
     let created = 0;
 
     try {
-        const url = `https://graph.facebook.com/v20.0/${wabaId}/message_templates`;
+        // First, try to get the real WABA ID from the Phone Number ID
+        // The whatsappAccountId might be a Phone Number ID, not the WABA ID
+        let realWabaId = wabaId;
 
-        console.log("SyncTemplates: Fetching from Meta API", { url });
+        try {
+            console.log("SyncTemplates: Checking if ID is Phone Number ID or WABA ID...");
+            const phoneInfoUrl = `https://graph.facebook.com/v20.0/${wabaId}?fields=id,display_phone_number,verified_name`;
+
+            const phoneInfoResponse = await axios.get(phoneInfoUrl, {
+                params: { access_token: accessToken }
+            });
+
+            // If it has display_phone_number, it's a Phone Number ID, we need to get WABA ID
+            if (phoneInfoResponse.data.display_phone_number) {
+                console.log("SyncTemplates: Detected Phone Number ID, fetching WABA ID...");
+
+                // Get the WABA ID from phone number
+                const wabaUrl = `https://graph.facebook.com/v20.0/${wabaId}?fields=whatsapp_business_account`;
+                const wabaResponse = await axios.get(wabaUrl, {
+                    params: { access_token: accessToken }
+                });
+
+                if (wabaResponse.data.whatsapp_business_account?.id) {
+                    realWabaId = wabaResponse.data.whatsapp_business_account.id;
+                    console.log("SyncTemplates: Found WABA ID:", realWabaId);
+                }
+            }
+        } catch (phoneError: any) {
+            // If query fails, assume the ID is already a WABA ID
+            console.log("SyncTemplates: Could not determine ID type, using as WABA ID");
+        }
+
+        const url = `https://graph.facebook.com/v20.0/${realWabaId}/message_templates`;
+
+        console.log("SyncTemplates: Fetching from Meta API", { url, realWabaId });
 
         const response = await axios.get(url, {
             params: {
