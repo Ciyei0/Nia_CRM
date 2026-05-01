87fbec8960d996d852df5b927bbf37223e316bb3
HugoDonator <hdonator15@gmail.com>
2026-04-30 22:50:31 -0400

    delete search bar 2

--- frontend/src/components/TicketsManagerTabs/index.js
+++ frontend/src/components/TicketsManagerTabs/index.js
@@ -320,20 +320,7 @@ const TicketsManagerTabs = () => {
         }}
       />
 
-      <div className={classes.searchContainer}>
-        <div className={classes.serachInputWrapper}>
-            <div className={classes.searchIconWrapper}>
-                <SearchIcon className={classes.searchIcon} />
-            </div>
-            <input
-              type="text"
-              placeholder={i18n.t("tickets.search.placeholder")}
-              className={classes.searchInput}
-              value={searchParam}
-              onChange={handleSearch}
-            />
-        </div>
-      </div>
+
 
       <div style={{ display: 'flex', gap: '10px', padding: '0 12px' }}>
         <div style={{ flex: 1 }}>
